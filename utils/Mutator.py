e8dae93206d91fa0ad32ac38c0e12ce3977a2309
yhzwealth <yhzxb@outlook.com>
2024-05-08 12:26:25 +0800

    add mutators

--- utils/Mutator.py
+++ utils/Mutator.py
@@ -2,21 +2,34 @@ import random
 from typing import Any
 
 
-# TODO
+def insert_random_character(s: str) -> str:
+    """Returns s with a random character inserted"""
+    # TODO
+
+
+def delete_random_character(s: str) -> str:
+    """Returns s with a random character deleted"""
+    # TODO
+
+
+def flip_random_character(s: str) -> str:
+    """Returns s with a random bit flipped in a random position"""
+    # TODO
+
 
 def duplicate_random_character(s: str) -> str:
     """Copy a range of characters."""
-    pass
+    # TODO
 
 
 def set_random_character(s: str) -> str:
     """Set a character to a random value"""
-    pass
+    # TODO
 
 
 def swap_random_character(s: str) -> str:
     """Swap 2 characters."""
-    pass
+    # TODO
 
 
 class Mutator:
@@ -24,7 +37,12 @@ class Mutator:
     def __init__(self) -> None:
         """Constructor"""
         self.mutators = [
-            # TODO
+            insert_random_character,
+            delete_random_character,
+            flip_random_character,
+            duplicate_random_character,
+            set_random_character,
+            swap_random_character
         ]
 
     def mutate(self, inp: Any) -> Any:  # can be str or Seed (see below)

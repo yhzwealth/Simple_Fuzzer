369294434854f5dd9be3025d49433243f0ff74e3
yhzwealth <yhzxb@outlook.com>
2024-05-08 12:24:27 +0800

    add mutators

--- utils/Mutator.py
+++ utils/Mutator.py
@@ -30,6 +30,21 @@ def flip_random_character(s: str) -> str:
     return s[:pos] + new_c + s[pos + 1:]
 
 
+def duplicate_random_character(s: str) -> str:
+    """Copy a range of characters."""
+    pass
+
+
+def set_random_character(s: str) -> str:
+    """Set a character to a random value"""
+    pass
+
+
+def swap_random_character(s: str) -> str:
+    """Swap 2 characters."""
+    pass
+
+
 class Mutator:
 
     def __init__(self) -> None:
